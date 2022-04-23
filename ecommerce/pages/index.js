73be441398088d43746abbe0d7a6c7add1740d6f
arod147 <code.alex.rodriguez@outlook.com>
2022-04-23 14:41:36 -0400

    Baisc home page done and product page

--- ecommerce/pages/index.js
+++ ecommerce/pages/index.js
@@ -6,18 +6,17 @@ import { client } from '../lib/client'
 const Home = ({ products, bannerData}) => {
   return (
     <>
-      <HeroBanner />
-      {console.log(products)}
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]}/>
       <div className='products-heading'>
         <h2>Best Selling Products</h2>
         <p>Speakers of many variations</p>
       </div>
 
       <div className='products-container'>
-        {products?.map((product) => product.name)}
+        {products?.map((product) => <Product key={product._id} product={product}/>)}
       </div>
 
-      <FooterBanner />
+      <FooterBanner footerBanner={bannerData && bannerData[0]}/>
     </>
   )
 };
